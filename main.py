a335e09ee5d01af04487e8d994805868b6e9394b
Peter B <122346661+peterbruder@users.noreply.github.com>
2025-07-23 13:59:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ st.markdown(
     unsafe_allow_html=True
 )
 # Große Logos zentriert in voller Breite
-st.sidebar.image("LOGO FH_Muenster_Transparent.png", use_container_width=True)
+st.sidebar.image("Abbildungen/LOGO FH_Muenster_Transparent.png", use_container_width=True)
 st.sidebar.image("Abbildungen/Logo_FGV.png", use_container_width=True)
 st.sidebar.image("Abbildungen/Logo_FB_ETI_2.svg", use_container_width=True)
 
